e5d52f7937a3417ad55bcca4824c636ee03693f2
Shubham Saxena <shubhsaxena22@gmail.com>
2020-06-07 10:40:00 +0530

    updated server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' })
 const app = require('./app')
 // console.log(process.env);
-const port = 3000;
+const port = process.env.PORT || 3000;
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 mongoose.connect(DB, {
     useNewUrlParser: true,
